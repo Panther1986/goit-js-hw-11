011bc6ef483ba959617f4c4c782353127bf4afe2
Panther1986 <polzovatel77@gmail.com>
2024-01-31 23:22:43 +0200

    update5

--- src/main.js
+++ src/main.js
@@ -1,8 +1,12 @@
 //import iziToast from "izitoast";
 //import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const inputElem = document.querySelector('.search-input');
 const formElem = document.querySelector('.search-form');
+const imagesContainer = document.querySelector('.gallery');
+
 
 
 
@@ -12,8 +16,36 @@ fetch('https://pixabay.com/api/?key=42127236-8bfdbbfbeed8a2dadaca720e8&q=cat&ima
     }
     return response.json();
 })  .then(data => {
-    // Data handling
+  const markup = generateGalleryMarkup(data);
+  console.log(markup);
   })
   .catch(error => {
     // Error handling
   });
+
+  
+
+function generateGalleryMarkup() {
+    return images.map(() => {
+        return `
+   <li class="gallery-item">
+  <a class="gallery-link" href="{{largeImageURL}}">
+    <img class="gallery-image" src="{{webformatURL}}" alt="{{tags}}" />
+  </a>
+  <p class="galerry-text">likes: {{likes}}</p>
+  <p class="galerry-text">views: {{views}}</p>
+  <p class="galerry-text">comments: {{comments}}</p>
+  <p class="galerry-text">downloads: {{downloads}}</p>
+</li>
+    `;
+    }).join('');
+}
+// const imageMarkup = generateGalleryMarkup(images);
+// imagesContainer.insertAdjacentHTML('beforeend', imageMarkup);
+
+
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+});
