d290381ec6ad94a1823a150c4ba17d7b954acbb5
Panther1986 <polzovatel77@gmail.com>
2024-01-31 10:10:25 +0200

    update2

--- src/main.js
+++ src/main.js
@@ -1,2 +1,13 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+
+const options = {
+    method: "GET"
+};
+
+fetch("<https://pixabay.com/api/videos/?key={ user_id:42127236 }&q=yellow+flowers&image_type=photo>", options).then(response => {
+    if (!response.ok) {
+        throw new Error(response.status);
+    }
+    return response.json();
+})
